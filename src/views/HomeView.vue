f2ae71956d3eb660374219beace0b829671b4cdd
+ <+>
2024-10-24 20:36:29 +0630

    fetching posts data with async and await function in composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,35 @@
 import { ref } from 'vue';
 import PostsList from '../components/PostsList.vue';
 
-let posts = ref([
-  
-])
+let posts = ref([]);
+let error = ref("");
 
+let load = async () => {
+  try {
+    let response = await fetch("http://localhost:3000/posts");
+    if (response.status === 404) {
+      throw new Error("Not found URL");
+    }
+    let datas = await response.json();
+    posts.value = datas;
+  } catch (err) {
+    error.value = err.message;
+  }
+}
+
+load();
 </script>
 
 <template>
   <div class="home">
-    <PostsList :posts="posts"></PostsList>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <div v-if="posts.length > 0">
+      <PostsList :posts="posts"></PostsList>
+    </div>
+    <div v-else>
+      loading....
+    </div>
   </div>
 </template>
